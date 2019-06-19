562e1bc35a0de36d4a13b2f69d30cc8e6722e371
shibao <shibaohong@outlook.com>
2019-06-20 00:07:05 +0800

    Add License and Operating System info into classifiers of setup.py

--- setup.py
+++ setup.py
@@ -22,5 +22,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
