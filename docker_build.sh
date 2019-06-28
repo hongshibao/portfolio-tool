48cdf00dbd93df04838fa37d5d7bf3e4640fb3cb
shibao <shibaohong@outlook.com>
2019-06-28 18:20:16 +0800

    Add DOCKER_IMAGE env var to overwrite hardcoded image name

--- docker_build.sh
+++ docker_build.sh
@@ -1,5 +1,6 @@
+: "${DOCKER_IMAGE:=thinkpoet/portfolio-tool}"
 : "${DOCKER_TAG:=test}"
 : "${BASE_BUILD_IMAGE:=thinkport/portfolio-base:latest}"
 
 docker build -f docker/Dockerfile.base -t "${BASE_BUILD_IMAGE}" .
-docker build -f docker/Dockerfile -t thinkpoet/portfolio-tool:${DOCKER_TAG} --build-arg BASE_IMAGE="${BASE_BUILD_IMAGE}" .
+docker build -f docker/Dockerfile -t ${DOCKER_IMAGE}:${DOCKER_TAG} --build-arg BASE_IMAGE="${BASE_BUILD_IMAGE}" .
