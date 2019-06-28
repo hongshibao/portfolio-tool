850d3034512da1fd997df6d70f3af9e2e734305d
shibao <shibaohong@outlook.com>
2019-06-29 00:47:09 +0800

    Add command to generate requirements.txt

--- docker_build.sh
+++ docker_build.sh
@@ -2,5 +2,7 @@
 : "${DOCKER_TAG:=test}"
 : "${BASE_BUILD_IMAGE:=thinkport/portfolio-base:latest}"
 
+pipenv lock -r | tail -n +2 > requirements.txt
+
 docker build -f docker/Dockerfile.base -t "${BASE_BUILD_IMAGE}" .
 docker build -f docker/Dockerfile -t ${DOCKER_IMAGE}:${DOCKER_TAG} --build-arg BASE_IMAGE="${BASE_BUILD_IMAGE}" .
