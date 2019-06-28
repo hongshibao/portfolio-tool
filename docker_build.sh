5b35b7d8a25c32f4f1c6f5f535eccb96ab5f3045
shibao <shibaohong@outlook.com>
2019-06-28 18:08:23 +0800

    Add BASE_BUILD_IMAGE env var to remove hardcode

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,5 @@
 : "${DOCKER_TAG:=test}"
+: "${BASE_BUILD_IMAGE:=thinkport/portfolio-base:latest}"
 
-docker build -f docker/Dockerfile.base -t thinkpoet/portfolio-tool-base:latest .
-docker build -f docker/Dockerfile -t thinkpoet/portfolio-tool:${DOCKER_TAG} --build-arg BASE_IMAGE="thinkpoet/portfolio-tool-base:latest" .
+docker build -f docker/Dockerfile.base -t "${BASE_BUILD_IMAGE}" .
+docker build -f docker/Dockerfile -t thinkpoet/portfolio-tool:${DOCKER_TAG} --build-arg BASE_IMAGE="${BASE_BUILD_IMAGE}" .
